de80f233d9709f193fe09f394a388cb95e76384e
Jason Farquhar <jdrf@zepler.org>
2016-07-26 01:43:27 +0200

    reduce verbosity when training

--- matlab/signalProc/apply_erp_clsfr.m
+++ matlab/signalProc/apply_erp_clsfr.m
@@ -33,7 +33,7 @@ if ( isfield(clsfr,'detrend') && clsfr.detrend ) % detrend over time
     if ( verb>0 ) fprintf('1) Detrend\n'); end
     X=detrend(X,2); % detrend over time
   elseif ( isequal(clsfr.detrend,2) )
-    if ( verb>0) fprintf('1) Center\n'); end
+    if ( verb>0 ) fprintf('1) Center\n'); end
     X=repop(X,'-',mean(X,2));
   end
 end 
