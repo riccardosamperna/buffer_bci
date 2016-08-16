047bbda46f86d190b85e98cc86d8aec62ab8f898
Jason Farquhar <jdrf@zepler.org>
2016-08-16 23:11:04 +0200

    adaptive whitener fixes

--- matlab/signalProc/train_ersp_clsfr.m
+++ matlab/signalProc/train_ersp_clsfr.m
@@ -23,7 +23,7 @@ function [clsfr,res,X,Y]=train_ersp_clsfr(X,Y,varargin)
 %  width_ms  - [float] width in millisecs for the windows in the welch spectrum (250)
 %              estimation.  
 %              N.B. the output frequency resolution = 1000/width_ms, so 4Hz with 250ms
-%  spatialfilter -- [str] one of 'slap','car','none','csp','ssep'              ('slap')
+%  spatialfilter -- [str] one of 'slap','car','none','csp','ssep','trwht'              ('slap')
 %       WARNING: CSP is particularly prone to *overfitting* so treat any performance estimates with care...
 %  badchrm   - [bool] do we do bad channel removal    (1)
 %  badchthresh - [float] threshold in std-dev units to id channel as bad (3.5)
@@ -65,7 +65,7 @@ function [clsfr,res,X,Y]=train_ersp_clsfr(X,Y,varargin)
 %  Y       -- [ppepoch x 1] pre-processed labels (N.B. will have diff num examples to input!)
 opts=struct('classify',1,'fs',[],'timeband_ms',[],'freqband',[],...
             'width_ms',500,'windowType','hanning','aveType','amp',...
-            'detrend',1,'spatialfilter','slap',...
+            'detrend',1,'spatialfilter','slap','adaptspatialfilt',[],...
             'badchrm',1,'badchthresh',3.1,'badchscale',2,...
             'badtrrm',1,'badtrthresh',3,'badtrscale',2,...
             'ch_pos',[],'ch_names',[],'verb',0,'capFile','1010','overridechnms',0,...
@@ -123,9 +123,9 @@ end
 
 %3.a) Spatial filter/re-reference (data-dependent-unsupervised)
 R=[];
-if ( size(X,1)> 5 && any(strcmpi(opts.spatialfilter,{'wht','whiten'})) ) 
+if ( size(X,1)>=4 && any(strcmpi(opts.spatialfilter,{'wht','whiten','trwht','adaptspatialfilt'})) ) 
   fprintf('3) whiten\n');
-  R=whiten(X,1,1,0,0,1); % symetric whiten	 
+  [R,Sigma]=whiten(X,1,1,0,0,1); % symetric whiten	 
 end
 
 %2.2) time range selection
@@ -138,7 +138,7 @@ if ( ~isempty(opts.timeband_ms) )
 end
 
 %3.b) Spatial filter/re-reference (data-independent / supervised)
-if ( size(X,1)> 5 ) % only spatial filter if enough channels
+if ( size(X,1)>=4 ) % only spatial filter if enough channels
   sftype=lower(opts.spatialfilter);
   switch ( sftype )
    case 'slap';
@@ -151,7 +151,7 @@ if ( size(X,1)> 5 ) % only spatial filter if enough channels
    case 'car';
     fprintf('3) CAR\n');
     R=eye(size(X,1))-(1./size(X,1));
-   case {'whiten','wht'};
+   case {'whiten','wht','trwht','adaptspatialfilt'};
 	  % N.B. done before time-range selection so has access to artifact information
    case {'csp','csp1','csp2','csp3'};
     fprintf('3) csp\n');
@@ -323,7 +323,11 @@ clsfr.fs          = fs;   % sample rate of training data
 clsfr.detrend     = opts.detrend; % detrend?
 clsfr.isbad       = isbadch;% bad channels to be removed
 clsfr.spatialfilt = R;    % spatial filter used for surface laplacian
-clsfr.adaptspatialfilt=[]; % no adaption
+if ( any(strcmp(opts.spatialfilter,{'trwht','adaptspatialfilter'})) ) % configure for apaptive use later
+  clsfr.spatialfilt=[];
+  clsfr.adaptspatialfilt=opts.adaptspatialfilt; % no adaption
+  clsfr.chCov      =Sigma;
+end
 
 clsfr.filt        = []; % DUMMY -- so ERP and ERSP classifier have same structure fields
 clsfr.outsz       = []; % DUMMY -- so ERP and ERSP classifier have same structure fields
