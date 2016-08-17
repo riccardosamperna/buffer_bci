8168f3fc3ecf35477e1db1963fbd7f543af0f567
Jason Farquhar <jdrf@zepler.org>
2016-08-18 00:06:22 +0200

    added a run-way-style calibration stimulus

--- matlab/Cybathalon/imCalibrateRunwayStimulus.m
+++ matlab/Cybathalon/imCalibrateRunwayStimulus.m
@@ -1,184 +1,81 @@
+configRun=true;
 configureIM;
-initgetwTime;
-initsleepSec;
 
-% pre-build the time-line for the whole experiment
-tgtSeq=mkStimSeqRand(nSymbs,nSeq);
+[tgtSeq]=mkStimSeqRand(nSymbs,nSeq);
 % insert the rest between tgts to make a complete stim sequence + event seq with times
 trlEP   = (trialDuration+baselineDuration+intertrialDuration)./epochDuration;
-stimSeq = zeros(size(tgtSeq,1)+1,size(tgtSeq,2)*trlEP);
-%zeros(size(tgtSeq,1)+1,size(tgtSeq,2)*(trialDuration+baselineDuration+intertrialDuration)./epochDuration);										  
-stimTime=(0:size(stimSeq,2))*epochDuration; % stimulus times										  
-eventSeq=true(1,size(stimSeq,2));           % when to send events
-
-										  % the normal trials
-stimSeq(1:nSymbs,:)=reshape(repmat(tgtSeq,trlEP,1),size(tgtSeq,1),[]); % tgts
+stimSeq = reshape(repmat(tgtSeq,trlEP,1),size(tgtSeq,1),[]);
 stimSeq(nSymbs+1,:)=0; % add a stimseq for the rest/baseline cue
-										  % the baseline phase
-for ei=1:ceil(baselineDuration/epochDuration);
-  stimSeq(:,ei:trlEP:end)=0; % everybody else is off
-  stimSeq(nSymbs+1,ei:trlEP:end)=1; % rest is on
-end
-			% the RTB phase at end of every trial ends with a RTB = no stimulus & no event
-for ei=1:ceil(intertrialDuration/epochDuration);
-  stimSeq(:,trlEP+1-ei:trlEP:end)=0;
-  eventSeq(1,trlEP+1-ei:trlEP:end)=false; % don't send event
-end
+%zeros(size(tgtSeq,1)+1,size(tgtSeq,2)*(trialDuration+baselineDuration+intertrialDuration)./epochDuration);
+										  % Every trial starts with a baseline cue
+stimSeq(:,1:trlEP:end)=0; stimSeq(nSymbs+1,1:trlEP:end)=1;
+										  % Every trial ends with a RTB = no stimulus
+stimSeq(:,trlEP:trlEP:end)=0;
 
-										  % setup image to hold the task runway
-visDur   = 10;
-% number frames store in the visible window, padded with enough extra to not need updating during trial
-visFrames= (visDur+trialDuration*2)./frameDuration; 
-visImg   = zeros(nSymbs,visFrames,3); % rgb image to render
-visT0    = 0; % absolute time visible fragement of the image starts
-visEnd   = 0; % index of the end of the valid part of the image
+										  % stimulus times
+stimTime=(0:size(stimSeq,2))*epochDuration;
+
+% render loop
+visDur   =10;
+visFrames=(visDur+trialDuration)./frameDuration; % number frames store in the visible window
+visImg   =zeros(nSymbs,visFrames,3); % rgb image to render
 
 
-% make the stimulus
-%figure;
-fig=figure(2);
-set(fig,'Name','Imagined Movement','color',winColor,'menubar','none','toolbar','none','doublebuffer','on');
+
 clf;
-ax=axes('position',[0.025 0.025 .95 .95],'units','normalized','visible','off','box','off',...
-        'xtick',[],'xticklabelmode','manual','ytick',[],'yticklabelmode','manual',...
-        'color',winColor,'DrawMode','fast','nextplot','replacechildren',...
-        'xlim',axLim,'xlimmode','manual','ylim',axLim,'ylimmode','manual','Ydir','normal');
-set(fig,'Units','pixel');wSize=get(fig,'position');set(fig,'units','normalized');% win size in pixels
-
-clear h;
-% setup for the runway: N.B. xdata/ydata are the centers of the pixels!
-xdata=[axLim(1)+.3*diff(axLim) axLim(2)]; ydata=axLim;
-h(1)=image('xdata',mean(xdata)+diff(xdata)/2*[-1 1]*(visFrames-1)/visFrames,...
-			  'ydata',mean(ydata)+diff(ydata)/2*[-1 1]*(nSymbs-1)/nSymbs,'cdata',repmat(shiftdim(bgColor(:),-2),[nSymbs,visFrames,1])); % start 10% into the axes
-imgh=h(1);
-										  % add labels for each row.
-ylim=get(ax,'ylim'); ypos=linspace(ylim(1),ylim(2),nSymbs+2); ypos=ypos(2:end-1);
-for si=1:nSymbs;
-  snm=sprintf('%d',si);
-  if ( ~isempty(symbCue) ) snm=sprintf('%d %s',si,symbCue{si}); end;
-  h(si+1)=text(axLim(1)+.3*diff(axLim),ypos(si),snm,...
-					'HorizontalAlignment', 'right', 'VerticalAlignment', 'middle',...
-					'fontunits','pixel','fontsize',.08*wSize(4),...
-					'color',txtColor,'visible','on');
-end
-
-										  % clear the display
+imgh=image(visImg(:,1:visDur/frameDuration,:));
 set(gca,'visible','off');
 
-%Create a text object with no text in it, center it, set font and color
-txthdl = text(mean(get(ax,'xlim')),mean(get(ax,'ylim')),' ',...
-				  'HorizontalAlignment', 'center', 'VerticalAlignment', 'middle',...
-				  'fontunits','pixel','fontsize',.05*wSize(4),...
-				  'color',txtColor,'visible','off');
-
-% text object for the experiment progress bar
-progresshdl=text(axLim(1),axLim(2),sprintf('%2d/%2d',0,size(stimSeq,2)),...
-				  'HorizontalAlignment', 'left', 'VerticalAlignment', 'top',...
-				  'fontunits','pixel','fontsize',.05*wSize(4),...
-				  'color',txtColor,'visible','on');
-
-% wait for starting button press
-set(txthdl,'string', 'Click mouse when ready', 'visible', 'on'); drawnow;
-waitforbuttonpress;
-set(txthdl,'visible', 'off'); drawnow;
-
-sendEvent('stimulus.training','start');
-
-
-
-% play the stimulus
 % animation loop
-t0=getwTime(); % absolute start time for the experiment
-waitforkeyTime=getwTime()+calibrateMaxSeqDuration;
-for ei=1:size(stimSeq,2);
-
-  % update progress bar
-  set(progresshdl,'string',sprintf('%2d/%2d',ei,size(stimSeq,2)));
-
-
-  % Give user a break if too much time has passed
-  if ( getwTime() > waitforkeyTime )
-	 b0=getwTime();
-	 set(txthdl,'string', {'Break between blocks.' 'Click mouse when ready to continue.'}, 'visible', 'on');
-	 drawnow;
-	 waitforbuttonpress;
-	 set(txthdl,'visible', 'off');
-	 drawnow;
-	 waitforkeyTime=getwTime()+calibrateMaxSeqDuration;
-	 sleepSec(intertrialDuration);
-
-	 % update the start time, as if started later to compensate for the time spent waiting
-	 t0 = t0+getwTime()-b0;;
-  end
+t0=getwTime(); % start sequence
+visT0 = 0;
+visEnd= 0; % end of visible image as [index, time]
+for ei=1:size(stimTime,2);
 
 										  % render stimSeq into the visImage
 										  % find epoch to start with
-  epI=[];
+  tfi=visT0+visEnd*frameDuration;
+  epI=find(tfi>stimTime,1,'last')+1; if ( isempty(epI) ) epI=1; end;
   for fi=visEnd+1:size(visImg,2); % render into the frameBuffer
 	 visImg(:,fi,:) = repmat(bgColor,size(visImg,1),1); % start as background color
-	 starttfi= visT0+(fi-1)*frameDuration; % start time for the current frame
-	 % find which epoch contains this frame
-	 if ( isempty(epI) ) 
-		epI=find(starttfi>=stimTime,1,'last');
-	 else
-		if ( starttfi>=stimTime(min(end,epI+1)) ) epI=epI+1; end % move to next epoch of needed
-	 end
-	 if ( ~isempty(epI) && epI<size(stimSeq,2) )
-		ss=stimSeq(:,epI);
-		if ( any(ss>0) ) % set target cols
-		  if ( ss(end) ) % rest
-			 visImg(:,fi,:)             =repmat(fixColor,nSymbs,1);
-		  else % tgt
-			 visImg(ss(1:nSymbs)>0,fi,:)=repmat(tgtColor,sum(ss(1:nSymbs)>0),1);
-		  end
+	 tfi= visT0+fi*frameDuration; % time for the current col
+	 if ( tfi>stimTime(epI+1) ) epI=epI+1; end % move to next epoch of needed
+	 ss=stimSeq(:,epI);
+	 if ( any(ss>0) ) % set target cols
+		if ( ss(end) ) % rest
+		  visImg(:,fi,:)             =repmat(fixColor,nSymbs,1);
+		else % tgt
+		  visImg(ss(1:nSymbs)>0,fi,:)=repmat(tgtColor,sum(ss(1:nSymbs)>0),1);
 		end
 	 end
   end
   visEnd=fi; % update the end valid-data indicator
   
-  % send the epoch events
-  if ( ~eventSeq(ei) ) % should we send an event
-	 set(txthdl,'string','','color',txtColor,'visible','on');		  
-  else
-	 if ( stimSeq(end,ei) ) % baseline epoch
-		sendEvent('stimulus.baseline','start');
-		if ( ~isempty(baselineClass) ) % treat baseline as a special class
-		  sendEvent('stimulus.target',baselineClass);
-		  if ( verb>1 ) set(txthdl,'string',baselineClass,'color',txtColor,'visible','on'); end;
-		else
-		  if ( verb>1 ) set(txthdl,'string','rest','color',txtColor,'visible','on'); end;
-		end
-	 else % target action epoch
-		tgtIdx=find(stimSeq(:,ei)>0);
-		if ( ~isempty(symbCue) )
-		  tgtNm = '';
-		  for ti=1:numel(tgtIdx);
-			 if(ti>1) tgtNm=[tgtNm ' + ']; end;
-			 tgtNm=sprintf('%s%d %s ',tgtNm,tgtIdx,symbCue{tgtIdx});
-		  end
-		else
-		  tgtNm = sprintf('%d',tgtIdx); % human-name is position number
-		end
-		fprintf('%d) tgt=%10s : \n',ei,tgtNm);
-		if ( verb>1 ) set(txthdl,'string',tgtNm,'color',txtColor,'visible','on'); end
-		sendEvent('stimulus.target',tgtNm);
+  fprintf('%d) Tgt=%g True=%g\n',ei,stimTime(ei),getwTime()-t0);
+  animateDuration = stimTime(ei+1)-stimTime(ei);
+  a0=t0+visT0; % absolute time of start of this sequence
+  elapsedTime=getwTime()-a0;ofi=-1;fi=ceil(elapsedTime/frameDuration);
+  while ( elapsedTime<animateDuration )
+	 if ( ofi~=fi ) % only actually draw if something changed
+		set(imgh,'cdata',visImg(:,fi+(1:visDur/frameDuration),:)); % update display
+		drawnow;
+	 end
+	 if ( verb>1 ) fprintf('%d) f=%d t=%g t_e=%g\n',ei,fi,getwTime()-t0,getwTime()-a0) end;
+	 sleepSec(frameDuration*.5);
+	 f0=getwTime();
+	 elapsedTime = f0-a0; % time left to run in this trial
+	 ofi=fi;
+	 fi=ceil(elapsedTime/frameDuration); % get closest frame time
+	 if ( fi-ofi>1 )
+		fprintf('%d) Dropped frames %d...\n',fi,ofi-fi);
 	 end
   end
+  set(imgh,'cdata',visImg(:,fi+(1:visDur/frameDuration),:)); % update display
+  drawnow;
 
-  % run the animation
-  et=getwTime()-t0;
-  fprintf('%d) Tgt=[%6.2f-%6.2f]\tTrue=%6.2f\tdiff=%g\n',ei,stimTime(ei:ei+1),et,et-stimTime(ei));
-  animateDuration = stimTime(ei+1)-stimTime(ei);
-  set(h,'visible','on');
-  % call the function to run the actual animation
-  animateRunway
+										  % shift the image and update visEnd
+  visImg(:,1:(visEnd-fi+1),:)=visImg(:,fi:visEnd,:);
+  visEnd=visEnd-fi+1; % shift back the end of correct info
+  visT0 =visT0+((fi-1)*frameDuration); % shift start of the matrix forward
   
-end
-% end training marker
-sendEvent('stimulus.training','end');
-
-if ( ishandle(fig) ) % thanks message
-  set(h,'visible','off');
-set(txthdl,'string',{'That ends the training phase.','Thanks for your patience'}, 'visible', 'on', 'color',[0 1 0]);
-pause(3);
 end
