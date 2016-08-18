b0a860ba185e98d6589b2e38f1beff110e016729
Jason Farquhar <jdrf@zepler.org>
2016-08-18 10:48:00 +0200

    fixes for the adaptive filter stuff

--- matlab/Cybathalon/configureIM.m
+++ matlab/Cybathalon/configureIM.m
@@ -80,22 +80,29 @@ animateStep  = diff(axLim)*.01; % amount by which to move point per-frame in fix
 trlen_ms      =epochDuration*1000; % how much data to use in each classifier training example
 offset_ms     =[250 250]; % give .25s for user to start/finish
 calibrateOpts ={'offset_ms',offset_ms};
+adaptHalfLife_ms = 10*1000; %10s
 
 										% classifier training options
 welch_width_ms=250; % width of welch window => spectral resolution
 step_ms       =welch_width_ms/2;% N.B. welch defaults=.5 window overlap, use step=width/2 to simulate
+trialadaptfactor=exp(log(.5)/(adaptHalfLife_ms/trlen_ms)); % adapt rate when apply per-trial
+contadaptfactor =exp(log(.5)/(adaptHalfLife_ms/welch_width_ms)); % adapt rate when apply per welch-win
+
 %trainOpts={'width_ms',welch_width_ms,'badtrrm',0}; % default: 4hz res, stack of independent one-vs-rest classifiers
-trainOpts={'width_ms',welch_width_ms,'badtrrm',0,'spatialfilter','wht','objFn','mlr_cg','binsp',0,'spMx','1vR'}; % whiten + direct multi-class training
+%trainOpts={'width_ms',welch_width_ms,'badtrrm',0,'spatialfilter','wht','objFn','mlr_cg','binsp',0,'spMx','1vR'}; % whiten + direct multi-class training
+trainOpts={'width_ms',welch_width_ms,'badtrrm',0,'spatialfilter','trwht','adaptivespatialfilt',trialadaptfactor,'objFn','mlr_cg','binsp',0,'spMx','1vR'}; % adaptive-whiten + direct multi-class training
 %trainOpts = {'spType',{{1 3} {2 4}}}; % train 2 classifiers, 1=N vs S, 2=E vs W
 
 % Epoch feedback opts
 %%0) Use exactly the same classification window for feedback as for training, but
 %%   but also include a bias adaption system to cope with train->test transfer
-earlyStopping=true;
+earlyStopping=false;%true;
 epochFeedbackOpts={}; % raw output
 %epochFeedbackOpts={'predFilt',@(x,s) biasFilt(x,s,exp(log(.5)/50))}; % bias-apaption
 % Epoch feedback with early-stopping, config using the user feedback table
 userFeedbackTable={'epochFeedback_es' 'cont' {'predFilt',@(x,s,e) gausOutlierFilt(x,s,3.0,trialDuration*1000./step_ms),'trlen_ms',welch_width_ms}};
+% Epoch feedback with early-stopping, (cont-classifer, so update adaptive whitener constant)
+userFeedbackTable={'epochFeedback_es' 'cont' {'predFilt',@(x,s,e) gausOutlierFilt(x,s,3.0,trialDuration*1000./step_ms),'trlen_ms',welch_width_ms,'adaptivespatialfilt',contadaptfactor}};
 
 
 % different feedback configs (should all give similar results)
@@ -109,6 +116,8 @@ stimSmoothFactor= 0; % additional smoothing on the stimulus, not needed with 3s 
 %%2) Classify every welch-window-width (default 250ms), prediction is running average of full trials worth of data, no-bias adaptation
 %% N.B. this is numerically identical to option 1) above, but computationally *much* cheaper 
 contFeedbackOpts ={'predFilt',-(trlen_ms/step_ms),'trlen_ms',welch_width_ms};
+% classify every welch-window-width, update adapt-filt hl w.r.t. shorter input windows
+contFeedbackOpts ={'predFilt',-(trlen_ms/step_ms),'trlen_ms',welch_width_ms,'adaptivespatialfilt',exp(log(.5)/(adaptHalfLife_ms/welch_width_ms))};
 
 
 %%3) Classify every welch-window-width (default 500ms), with bias-adaptation
