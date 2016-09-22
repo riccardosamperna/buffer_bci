63c8daf6e0349c69a9076ead71f6235ec241ce5b
Jason Farquhar <jdrf@zepler.org>
2016-09-22 19:38:46 +0200

    help text and support for leave-label-out folding

--- matlab/utilities/gennFold.m
+++ matlab/utilities/gennFold.m
@@ -3,6 +3,8 @@ function [foldIdxs]=gennFold(Y,nFold,varargin)
 %
 %  Generate nFolds X repeats of the given data with equal proportions of
 %  each class.
+%    foldIdxs is an [N x nFolds*nRepeats] indicator matrix where:
+%             -1 = training point, 0 = excluded point, +1 = testing point
 %  N.B. Folds   -- are non-overlapping splits of the data
 %       Repeats -- are sets of folds (and hence can overlap)
 %
@@ -13,6 +15,8 @@ function [foldIdxs]=gennFold(Y,nFold,varargin)
 %  nFold -- [1x1] number of folds to generate
 %            OR
 %           'loo' for leave-one-out folding, i.e. where each example is a test example exactly once
+%           'llo' for leave-label-out folding, i.e. 1 fold for each unique label value
+%         N.B. negative value (-ve) means use non-overlapping training sets.  +ve=non-overlapping test
 % Options:
 %   dim      - [ind] dimension of Y which contains subProblems (ndims(Y))
 %   nFold   -- [int] same as nFold input
@@ -24,6 +28,7 @@ function [foldIdxs]=gennFold(Y,nFold,varargin)
 %   foldSize - [1x1] number of points in each fold (N/nFold)
 %              OR
 %              [nClass x 1] number of points of each class for each fold
+%             N.B. -ve=set the training-set size,  +ve=set the test-set size
 %   randseed - [1x1] seed for the random number generator
 %   repeats  - [1x1] how many sets of nFolds to generate (1)
 %   zeroLab  - [bool] do we treat 0 labels as ignored or true labels (0)
@@ -47,18 +52,29 @@ function [foldIdxs]=gennFold(Y,nFold,varargin)
 % made of any changes that have been made. This software and
 % documents are distributed without any warranty, express or
 % implied
-if ( nargin < 2 ); nFold=10; 
-elseif ( ischar(nFold) && ~strcmp(nFold,'loo') ); varargin={nFold varargin{:}}; nFold=[];
+if ( nargin < 2 ) nFold=10; 
+elseif ( ischar(nFold) && ~any(strcmp(nFold,{'loo','llo'})) ) varargin={nFold varargin{:}}; nFold=[];
 end;
-opts=struct('indepSP',0,'perm',0,'foldSize',[],'regress',0,'randseed',{[]},'repeats',1,'dim',[],'zeroLab',0,'nFold',[]);
+opts=struct('indepSP',0,'perm',0,'foldSize',[],'regress',0,'randseed',{[]},'repeats',[],'dim',[],'zeroLab',0,'nFold',[]);
 opts=parseOpts(opts,varargin);
-dim=opts.dim; if (isempty(dim) ); dim=ndims(Y); end;
-if ( isempty(nFold) ); nFold=opts.nFold; end;
-if ( isempty(nFold) && isempty(opts.foldSize) ); nFold=10; end;
+dim=opts.dim; if (isempty(dim) ) dim=ndims(Y); end;
+if ( isempty(nFold) ) nFold=opts.nFold; end;
+if ( isempty(nFold) && isempty(opts.foldSize) ) nFold=10; end;
 
 % deal with degenerate cases
-if ( nFold==1 );     foldIdxs = -ones(size(Y)); return;              % all training
-elseif ( nFold==0 ); foldIdxs =  ones(size(Y)); return;              % no folding, all testing
+if ( nFold==1 )     foldIdxs = -ones(size(Y)); return;              % all training
+elseif ( nFold==0 ) foldIdxs =  ones(size(Y)); return;              % no folding, all testing
+elseif ( isequal(nFold,'loo') || isequal(opts.foldSize,1) )         % Leave-one-out
+  szY=size(Y); 
+  foldIdxs=eye(prod(szY(setdiff(1:end,dim))))*2-1;
+  if ( numel(dim)>1) foldIdxs=reshape(foldIdxs,szY(setdiff(1:end,dim)),[]); end;
+  return;
+elseif ( isequal(nFold,'llo') ) % leave label out
+  key=unique(Y(:));
+  foldIdxs=-ones([prod(size(Y)),numel(key)]);
+  for fi=1:numel(key); foldIdxs(Y(:)==key(fi),fi)=1; end;
+  foldIdxs = reshape(foldIdxs,[size(Y) numel(key)]);
+  return;  
 end
 
 % set rand state
@@ -67,10 +83,13 @@ if(~isempty(opts.randseed))
 end
 
 if ( size(Y,dim)==1 && isequal(dim,2) ) %&& ~all(Y(:)==-1 | Y(:)==0 | Y(:)==1) ) % label set... 
-   if ( opts.regress ); Y=true(size(Y)); else; Y=lab2ind(Y,[],[],opts.zeroLab,0); end; 
+   if ( opts.regress ) Y=true(size(Y)); else Y=lab2ind(Y,[],[],opts.zeroLab,0); end; 
 end 
 
-  % deal with n-d inputs
+% deal with n-d inputs
+if( ~isequal(dim,ndims(Y)) ) % transpose if sub-probs in the 1st dimension
+  Y=permute(Y,[setdiff(1:ndims(Y),dim) dim]); dim=ndims(Y)-(0:numel(dim)-1); 
+end;
 szY=size(Y); szY(end+1:dim)=1;
 Y  =reshape(Y,prod(szY(1:dim-1)),szY(dim)); % make n-d inputs 2-d
 
@@ -79,19 +98,19 @@ Y  =reshape(Y,prod(szY(1:dim-1)),szY(dim)); % make n-d inputs 2-d
 oY = Y;
 Y  = -ones(size(Y,1),size(Yu,1));
 for ci=1:size(Y,2); Y(idx==ci,ci)=1; end;
-if ( ~opts.zeroLab ); Y(:,all(Yu==0,2))=[]; Yu(all(Yu==0,2),:)=[]; end; % remove zero labelled points
+if ( ~opts.zeroLab ) Y(:,all(Yu==0,2))=[]; Yu(all(Yu==0,2),:)=[]; end; % remove zero labelled points
 
 % deal with special types of call
 
 % #1: independent sub-problems are treated as set of calls
 if ( opts.indepSP && size(Y,2)>1 ) 
-   foldIdxs=zeros([size(Y),nFold],'single');
+   foldIdxs=zeros([size(Y),abs(nFold)],'single');
    for spi=1:size(Y,2);
       foldIdxs(:,spi,:)=gennFold(Y(:,spi),nFold,opts,'indepSP',0);
    end
 
 % #2: leave-one-out folding!
-elseif ( isequal(nFold,size(Y,1)) || strcmp(nFold,'loo') || (isempty(nFold) && isequal(opts.foldSize,1)) )
+elseif ( isequal(abs(nFold),size(Y,1)) || strcmp(nFold,'loo') || (isempty(nFold) && isequal(opts.foldSize,1)) )
    foldIdxs=-ones(size(Y,1)); foldIdxs(1:size(Y,1)+1:end)=1; 
 
 % #3: normal folding
@@ -99,7 +118,7 @@ else
 
   if ( ~isequal(opts.perm,0) ) %permutate the inputs if wanted 
     % generate a random permutation
-    if ( isequal(opts.perm,1) ); perm=randperm(size(Y,1)); 
+    if ( isequal(opts.perm,1) ) perm=randperm(size(Y,1)); 
     elseif( opts.repeats > 1 || numel(opts.perm)~=max(size(Y)) ) 
       error('Invalid permutation specfication');
     end;
@@ -112,28 +131,31 @@ else
     if( numel(fSize)==1 && size(Y,2)>1 ) % split proportionally between classes to sum to fSize
       fSize = fSize.*[sum(Y>0,1)./sum(Y(:)>0)]; 
     end
-    if ( isempty(nFold) ) 
-      nFold = floor(min(sum(Y>0,1)./fSize(:)')+1e-6); % N.B. correct for rounding errors before the floor
-    end;
+	 if ( all(fSize>sum(Y>0,1)/2) ) fSize=-(sum(Y>0,1)-fSize); end;
   else
     fSize = sum(Y>0,1)./nFold; % compute per-lab fold size
   end
 
-  if ( nFold > size(Y,1) ); error('More folds than datapoints!'); end;
+  if ( abs(nFold) > size(Y,1) ) error('More folds than datapoints!'); end;
+
+  % number of folds per repeat  
+  repFold = min(abs(nFold),floor(min(sum(Y>0,1)./abs(fSize(:))')+1e-6));
+  repeats = opts.repeats;
+  if ( isempty(repeats) ) repeats=ceil(nFold/repFold); end % spec from nFold
   
-  foldIdxs=genFold(Y(perm,:),nFold,fSize); % generate the first repeat
+  foldIdxs=genFold(Y(perm,:),repFold,fSize); % generate the first repeat
   foldIdxs(perm,:)=foldIdxs;         % undo perm to, store in orginal order
 
   % generate the repeats if wanted
-  for i=2:opts.repeats;
+  for i=2:repeats;
     perm=randperm(size(Y,1));                  % gen rand permutation
-    tfoldIdxs=genFold(Y(perm,:),nFold,fSize);  % generate this repeat
+    tfoldIdxs=genFold(Y(perm,:),repFold,fSize);  % generate this repeat
     tfoldIdxs(perm,:)=tfoldIdxs;               % undo permutation 
     foldIdxs=[foldIdxs tfoldIdxs];             % store result
   end
 end
 
-if ( ~isempty(opts.randseed) ); rand('state',oseed); end; % restore rand state
+if ( ~isempty(opts.randseed) ) rand('state',oseed); end; % restore rand state
 
 if ( numel(szY)>2 ) % deal with n-d inputs, by converting back to n-d
    if ( opts.indepSP )
@@ -148,50 +170,58 @@ return;
 function [foldIdxs]=genFold(Y,nFold,fSize,fOffset)
 % Loop to actually generate the folds
 % convert binary as special case
-if( nargin<4 ); fOffset=[]; end;
-if( size(Y,2)==1 ); Y=[Y -Y]; end;
-if( numel(fSize)==1 ); fSize=fSize*[sum(Y>0,1)./sum(Y(:)>0)]; end;
+if( nargin<4 ) fOffset=[]; end;
+if( size(Y,2)==1 ) Y=[Y -Y]; end;
+if( numel(fSize)==1 ) fSize=fSize*[sum(Y>0,1)./sum(Y(:)>0)]; end;
 if( ~all(fSize==floor(fSize)) ) 
   % spread the non-integer part (roughly) equally over folds
   % N.B. doing this *right* to exactly equally spread examples is quite fiddly..
-  if ( isempty(fOffset) ); fOffset=randperm(size(Y,2))./size(Y,2); end;
+  if ( isempty(fOffset) ) fOffset=randperm(size(Y,2))./size(Y,2); end;
   fOffset(fSize==floor(fSize))=0; % no jitter if not needed
 else
   fOffset=zeros(size(Y,2),1);
 end; 
-foldIdxs=zeros([size(Y,1),nFold],'single');
+foldIdxs=zeros([size(Y,1),abs(nFold)],'single');
 for l=1:size(Y,2);
+  fSizel=abs(fSize(l));
   lPos=find(Y(:,l)>0); % get the pts in this class
-  if ( isempty(lPos) ); continue ; end;
+  if ( isempty(lPos) ) continue ; end;
   % assign points in this class to the corrospending folds.
-  for fold=1:nFold;
-     idx = floor((fold-1)*fSize(l)+(fold>1)*fOffset(l))+1:min(numel(lPos),floor(fold*fSize(l)+fOffset(l)));
+  for fold=1:abs(nFold);
+     idx = floor((fold-1)*fSizel+(fold>1)*fOffset(l))+1:min(numel(lPos),floor(fold*fSizel+fOffset(l)));
      % last fold gets all the rest of the data
-     if(fold==nFold && ~isempty(idx) && numel(lPos)<idx(end)+fSize(l)*.5 ) 
-        idx = floor((fold-1)*fSize(l)+(fold>1)*fOffset(l))+1:numel(lPos);
+     if(fold==abs(nFold) && ~isempty(idx) && numel(lPos)<idx(end)+fSizel*.5 ) 
+        idx = floor((fold-1)*fSizel+(fold>1)*fOffset(l))+1:numel(lPos);
      end
      % set the given points to be included in this fold. 
      foldIdxs(lPos,fold)=-1; % default to exclude these points
      foldIdxs(lPos(idx),fold)=1; % unless they're in this fold
   end
 end
+% negative fSize means invert sense of train/test
+if ( any(fSize(:) < 0) ) foldIdxs=-foldIdxs; end;
 return;
 
 %----------------------------------------------------------------------------
 function []=testCase()
 
 % test
+Y=randn(1000,1); Y(Y>.1)=1; Y(Y<-.1)=-1; Y(abs(Y)<.1)=0;
 
+foldIdxs=gennFold(Y,10,'perm',0);
 
 % validate the class counts
-for fi=1:size(z.foldIdxs,2); 
-   for spi=1:size(z.Y,2); 
-      nyi(:,spi,fi)=[sum(z.Y(z.foldIdxs(:,fi)<0,spi)>0)... % trn Y+
-                     sum(z.Y(z.foldIdxs(:,fi)>0,spi)>0)... % tst Y+
-                     sum(z.Y(z.foldIdxs(:,fi)<0,spi)<0)... % trn Y-
-                     sum(z.Y(z.foldIdxs(:,fi)>0,spi)<0)];  % tst Y-
-   end; 
+for fi=1:size(foldIdxs,2); 
+  fprintf('Fold(%2d)\t',fi);
+   for spi=1:size(Y,2); 
+      nyi(:,spi,fi)=[sum(Y(foldIdxs(:,fi)<0,spi)>0)... % trn Y+
+                     sum(Y(foldIdxs(:,fi)>0,spi)>0)... % tst Y+
+                     sum(Y(foldIdxs(:,fi)<0,spi)<0)... % trn Y-
+                     sum(Y(foldIdxs(:,fi)>0,spi)<0)];  % tst Y-
+      fprintf('sp(%2d) trn: %g+ %g-  tst: %g+ %g-\n',spi,nyi([1 3],spi,fi),nyi([2 4],spi,fi));
+    end; 
 end
+
 
 % Double nested cv
 Y = floor(rand(100,1)*(3-eps))+1;
