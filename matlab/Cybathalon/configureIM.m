3662fccc42ceaf489bfdd90f0a37dfc93401579e
c.verbaarschot@donders.ru.nl <Appel12Ko3!>
2016-11-02 10:51:53 +0100

    config for 3-class with implicit rest

--- matlab/Cybathalon/configureIM.m
+++ matlab/Cybathalon/configureIM.m
@@ -48,14 +48,14 @@ symbCue      ={'Feet' 'Left-Hand' 'Right-Hand'};
 %symbCue      ={'Alpha' 'Tongue' 'Hands'}; % config for JF
 %symbCue      ={'Tong' 'Voeten' 'Rechter-hand' 'Linker-hand'}; % config for P3
 nSymbs       =numel(symbCue); 
-baselineClass=[];%'99 Rest'; % if set, treat baseline phase as a separate class to classify
+baselineClass='99 Rest'; %[];% if set, treat baseline phase as a separate class to classify
 rtbClass     =[];%'999 rtb';% 'trialClass';% 'trialClass+rtb'; % 'rtb';% [];% if set post-trial is separate class also
 
 nSeq              =18*nSymbs; % 20 examples of each target
 epochDuration     =.75;
 trialDuration     =epochDuration*4*2; % 3*20 = 60 classification trials per class = 4.5s trials
-baselineDuration  =epochDuration*2*0;   % = 1.5s baseline
-intertrialDuration=epochDuration;%*2*0;   % = 1.5s post-trial
+baselineDuration  =epochDuration*2;   % = 1.5s baseline
+intertrialDuration=epochDuration*2; % = 1.5s post-trial
 feedbackDuration  =epochDuration*2;
 errorDuration     =epochDuration*2*3; %= 3s penalty for mistake
 calibrateMaxSeqDuration=150;        %= 2.5min between wait-for-key-breaks
